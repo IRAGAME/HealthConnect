14ea66b13df11c732b8f6025e565acd497046bc8
IRAGAME <iragimargos@gmail.com>
2026-02-12 04:25:29 -0500

    modif slug

--- backend/controllers/hospitalController.js
+++ backend/controllers/hospitalController.js
@@ -3,12 +3,16 @@ const db = require('../config/db');
 
 exports.createHospital = async (req, res) => {
   // On récupère les données envoyées depuis Postman
-  const { nom, adresse, telephone, email, latitude, longitude } = req.body;
+  const { nom, adresse, telephone, image_url } = req.body;
   
+  // Génération automatique du slug (ex: "Hôpital Central" -> "hopital-central")
+  // On met en minuscule, remplace les espaces par des tirets, et supprime les caractères spéciaux
+  const slug = nom.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+
   try {
     const newHospital = await db.query(
-      'INSERT INTO hopitaux (nom, adresse, telephone, email, latitude, longitude) VALUES ($1, , , , , ) RETURNING *',
-      [nom, adresse, telephone, email, latitude, longitude]
+      'INSERT INTO hopitaux (nom, slug, adresse, telephone, image_url) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [nom, slug, adresse, telephone, image_url]
     );
     res.status(201).json(newHospital.rows[0]);
   } catch (err) {
